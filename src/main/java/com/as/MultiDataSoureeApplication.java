bb9ffeae3d575521c969a118f1e16ae5aa64e628
sahil4683 <sahil4683@gmail.com>
2020-10-17 21:03:16 +0530

    first commit
    datasoucrce

---
+++ src/main/java/com/as/MultiDataSoureeApplication.java
@@ -0,0 +1,13 @@
+package com.as;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDataSoureeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDataSoureeApplication.class, args);
+	}
+
+}
